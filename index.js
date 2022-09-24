5cdd5acd7f88579284e4c3df88850c5728bcafc7
safwansadi <safwaniftekher@gmail.com>
2022-09-24 20:17:44 +0600

    Add chat feature

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const { PORT } = require("./utils/env");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const jwt = require("jsonwebtoken");
+const { JWT_PRIVATE_KEY } = require("./utils/env");
 
 app.use(express.static(path.join(__dirname, "uploads")));
 
@@ -10,7 +16,27 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 
-const server = app.listen(PORT, () => {
+io.use(function (socket, next) {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(
+      socket.handshake.query.token,
+      JWT_PRIVATE_KEY,
+      function (err, decoded) {
+        if (err) return next(new Error("Authentication error"));
+        socket.decoded = decoded;
+        next();
+      }
+    );
+  } else {
+    next(new Error("Authentication error"));
+  }
+}).on("connection", function (socket) {
+  socket.on("message", function (message) {
+    io.emit("message", message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
 
